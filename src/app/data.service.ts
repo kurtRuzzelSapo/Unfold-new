8a303c6e3d61e112ab13b1ca9b51a55c8bee6a28
kurtruzzelsapo <wkurtruzzelsapo@gmail.com>
2024-05-05 12:04:40 +0800

    Added a service

---
+++ src/app/data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Status } from './login/login.component';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) {}
+
+  apiURL: string = 'http://localhost/Unfold-API/api/';
+
+  sendRequest(endpoint: string, data: any): Observable<Status> {
+    return this.http.post<Status>(this.apiURL + endpoint, JSON.stringify(data));
+  }
+
+  getRequest(endpoint: string) {
+    const result = this.http.get(this.apiURL + endpoint);
+    return result;
+  }
+}
