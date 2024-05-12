f7917cb313602cde7108c647a8cdcecc0eb69047
ReyshellAnn <151622422+ReyshellAnn@users.noreply.github.com>
2024-05-12 14:23:23 +0800

    View Portfolio

--- src/app/topnav/topnav.component.ts
+++ src/app/topnav/topnav.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject} from '@angular/core';
 import {CookieService} from 'ngx-cookie-service';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-topnav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   providers: [CookieService],
   templateUrl: './topnav.component.html',
   styleUrl: './topnav.component.scss'
@@ -16,6 +19,9 @@ export class TopnavComponent implements OnInit {
   
   ngOnInit(): void {
     this.userDetails = JSON.parse(this.cookieService.get('user_details'));
+
+    
   }
 
+  
 }
